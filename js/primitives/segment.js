87f55d271329d8d30adaf617f706a0d0b3af0c19
Adam Brillon <adambrillon@gmail.com>
2025-04-06 18:43:23 -0400

    Update segment.js

--- js/primitives/segment.js
+++ js/primitives/segment.js
@@ -6,8 +6,8 @@ class Segment {
 
   draw(ctx, width = 2, color = "black") {
     ctx.beginPath();
-    ctx.strokeStyle = color;
     ctx.lineWidth = width;
+    ctx.strokeStyle = color;
     ctx.moveTo(this.p1.x, this.p1.y);
     ctx.lineTo(this.p2.x, this.p2.y);
     ctx.stroke();
